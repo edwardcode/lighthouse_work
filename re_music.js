97f8545216e327144e347bb16fc1c0923e5d9500
EDWARD YANG <eddiecody888@gmail.com>
2018-11-29 01:42:01 +0000

    removed a D

--- re_music.js
+++ re_music.js
@@ -9,7 +9,7 @@ var library = {
                    album: "WWDC 2003"},
             t03: { id: "t03",
                    name: "Four Thirty-Three",
-                   artist: "John Caged",
+                   artist: "John Cage",
                    album: "Woodstock 1952"}
           },
 
